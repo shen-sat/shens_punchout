18afccffca58cc36140cb018cb283b8766a0f8bf
shen-sat <shensat18@gmail.com>
2018-02-11 12:40:42 +0000

    enemy v enemy post_fight spec progress

--- lib/post_fight.rb
+++ lib/post_fight.rb
@@ -8,4 +8,15 @@ class PostFight
 	def level_up
 		winner.rank, loser.rank = loser.rank, winner.rank unless winner.rank < loser.rank
 	end
+
+	def amend_memories
+		if loser.is_a?(Enemy)
+			loser.memory[winner][1] += 1
+			loser.memory[winner][2] = false
+		end
+		if winner.is_a?(Enemy)
+			winner.memory[loser][0] += 1
+			winner.memory[loser][2] = true
+		end
+	end
 end
--- spec/post_fight_spec.rb
+++ spec/post_fight_spec.rb
@@ -7,6 +7,12 @@ let(:conor) 	{EnemyBuilder.new
 					.set_nickname("'The Notorious'")
 					.set_memory({})
 					.build}
+let(:max) 	{EnemyBuilder.new
+					.set_fname("Max")
+					.set_lname("Holloway")
+					.set_nickname("'Blessed'")
+					.set_memory({})
+					.build}
 let(:player)	{Player.new}
 	
 	context 'when player is one of the fighters' do 
@@ -34,11 +40,61 @@ let(:player)	{Player.new}
 			expect(conor.rank).to eq(5)
 		end
 
-		it 'should amend conor memory to reflect win for unknown player' do
+		it 'should amend conor memory to reflect loss to player' do
 			conor.memory[player] = [0,0,nil]
 			post_fight = PostFight.new(player,conor)
 			post_fight.amend_memories
-			expect(conor.memory[player]).to eq([0,1,true])
+			expect(conor.memory[player]).to eq([0,1,false])
+		end
+
+		it 'should amend conor memory to reflect win over player' do
+			conor.memory[player] = [1,2,false]
+			post_fight = PostFight.new(conor,player)
+			post_fight.amend_memories
+			expect(conor.memory[player]).to eq([2,2,true])
 		end
 	end
+
+	context 'when both fighters are enemies' do
+		it 'should take and store conor as winner and max as loser' do 
+			post_fight = PostFight.new(conor, max)
+			expect(post_fight.winner).to eq(conor)
+			expect(post_fight.loser).to eq(max) 
+		end
+
+		it 'should swap ranks for lower ranked max beating higher ranked conor' do 
+			max.rank = 3
+			conor.rank = 1
+			post_fight = PostFight.new(max,conor)
+			post_fight.level_up
+			expect(max.rank).to eq(1)
+			expect(conor.rank).to eq(3)
+		end
+
+		it 'should NOT swap ranks for higher ranked max beating lower ranked conor' do 
+			max.rank = 1
+			conor.rank = 3
+			post_fight = PostFight.new(max,conor)
+			post_fight.level_up
+			expect(max.rank).to eq(1)
+			expect(conor.rank).to eq(3)
+		end
+
+		it 'should amend conor memory to reflect loss to max' do
+			conor.memory[max] = [0,0,nil]
+			max.memory[conor] = [0,0,nil]
+			post_fight = PostFight.new(max,conor)
+			post_fight.amend_memories
+			expect(conor.memory[max]).to eq([0,1,false])
+		end
+
+		it 'should amend conor memory to reflect win over max' do
+			conor.memory[max] = [5,5,true]
+			max.memory[conor] = [5,5,false]
+			post_fight = PostFight.new(conor,max)
+			post_fight.amend_memories
+			expect(conor.memory[max]).to eq([6,5,true])
+		end
+
+	end 	
 end
